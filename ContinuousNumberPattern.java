22266f8e747c85a81cf9daae9841a8b661cf5756
Chirag Goel <goelchirag930@gmail.com>
2026-09-08 23:27:09 +0530

    Create ContinuousNumberPattern.java

---
+++ ContinuousNumberPattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter any no");
+    int n = sc.nextInt();
+        int num =1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+        
+            System.out.print(num++);
+        }
+        System.out.println();
+        
+    }
+    }
+}
